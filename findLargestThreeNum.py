4ad8752ec108ee07592714a109e6671050c604cf
Ankit Kumar <ak3434754@gmail.com>
2026-02-21 19:03:07 +0530

    Add function to find the largest of three numbers

---
+++ findLargestThreeNum.py
@@ -0,0 +1,12 @@
+a = 95
+b = 56
+c = 23
+if(a>b and a>c):
+    print(a," is Largest Number")
+elif(b>a and b>c):
+    print(b," is Largest Number")
+elif(c>a and c>b):
+    print(c," is Largest Number")
+else:
+    print(a," is Equal Number")
+        
